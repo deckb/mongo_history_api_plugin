f2ce347706843aabd0a9b89dca37c504387b2ab2
Buddy Deck <deck@eosnewyork.io>
2018-10-26 11:03:04 -0400

    fix missing history_api_plugin replacements

--- include/eosio/mongo_history_api_plugin/mongo_history_api_plugin.hpp
+++ include/eosio/mongo_history_api_plugin/mongo_history_api_plugin.hpp
@@ -4,7 +4,7 @@
  */
 
 #pragma once
-#include <eosio/history_plugin/history_plugin.hpp>
+#include <eosio/mongo_history_plugin/mongo_history_plugin.hpp>
 #include <eosio/chain_plugin/chain_plugin.hpp>
 #include <eosio/http_plugin/http_plugin.hpp>
 
@@ -14,12 +14,12 @@ namespace eosio {
 
    using namespace appbase;
 
-   class history_api_plugin : public plugin<history_api_plugin> {
+   class mongo_history_api_plugin : public plugin<mongo_history_api_plugin> {
       public:
         APPBASE_PLUGIN_REQUIRES((mongo_history_plugin)(mongo_db_plugin)(chain_plugin)(http_plugin))
 
-        history_api_plugin();
-        virtual ~history_api_plugin();
+        mongo_history_api_plugin();
+        virtual ~mongo_history_api_plugin();
 
         virtual void set_program_options(options_description&, options_description&) override;
 
